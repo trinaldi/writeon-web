5a3d0a87a5297da024412b389aeb29b354155cdd
Tiago Rinaldi <trinaldi@gmail.com>
2023-01-09 20:13:21 -0300

    Add tracking to title

--- src/components/UI/Typography/Title.js
+++ src/components/UI/Typography/Title.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = ({ children }) => {
   return (
-    <h1 className="f3 f2-m f1-l fw2 black-90 mv3">{ children }</h1>
+    <h1 className="tracked f3 f2-m f1-l fw2 black-90 mv3">{ children }</h1>
   )
 }
 
