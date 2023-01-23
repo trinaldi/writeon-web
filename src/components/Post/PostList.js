91b1ff80cef7b11a67b2a935f82939bf735a71d1
Tiago Rinaldi <trinaldi@gmail.com>
2023-01-23 10:26:20 -0300

    Move Header, Card to their own directory

--- src/components/Post/PostList.js
+++ src/components/Post/PostList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Post from './Post'
 import { useQuery } from '@apollo/client'
 import { POSTS_QUERY } from '../../graphql/queries/POSTS_QUERY';
+import Loader from '../UI/Loader/Loader';
 
 const PostList = () => {
   const { data, loading } = useQuery(POSTS_QUERY)
@@ -15,6 +16,9 @@ const PostList = () => {
           ))}
         </section>
       ) }
+
+
+      <Loader loading={loading} data={data} />
     </>
   )
 }
